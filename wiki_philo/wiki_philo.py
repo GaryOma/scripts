f77549800dca5f5f041cab0705f57213805af48b
SUBLET GARY p1506450 <gary.sublet@etu.univ-lyon1.fr>
2018-04-01 19:48:49 +0200

    ne va chercher que les bons liens

--- wiki_philo/wiki_philo.py
+++ wiki_philo/wiki_philo.py
@@ -1,23 +1,41 @@
 import urllib2
-import re
+import regex
 import sys
+import threading
+
+# (?s)<div(?:(?:[^(<]*+(?:(?!<div)|(?0))*+)*+)<\/div>
+
+# (<div(?:(?:(.+?(?=<div|<\/div))((?!<div)|(?1))*+)*?)<\/div>)
+
+# interessant :(?s)<div class=\"mw-parser-output\"(?:.*?)|(?:<a href=\"(?P<url>\/wiki\/(?:.*?)?)\")|(?:<div(?:(?:(?:.+?(?=<div|<\/div))(?:(?!<div)|(?0))*+)*?)<\/div>)
 
 def getPage(url):
     return urllib2.urlopen(url).read()
 
 def getPageName(page):
-    return re.search("<h1 id=\"firstHeading\" class=\"firstHeading\" lang=\"fr\">(?P<nomArticle>.*?)<\/h1>", page).group(1)
+    return regex.search("<h1 id=\"firstHeading\" class=\"firstHeading\" lang=\"fr\">(?P<nomArticle>.*?)<\/h1>", page).group(1)
+
+# def checkFirstLinkBottom(page):
+#     links = re.findall("(?s)<div class=\"mw-parser-output\"(?:.*?)|(?:<a href=\"(?P<url>/wiki/(?:.*?)?)\")|(?:<div(?:.*?)<\/div>|<div(?:.*?)<\/div>)|(?:<table class=\"infobox_v2\">(?:.*?)<\/table>)", page)
+#     for link in links:
+#         if not ':' in link:
+#             if link != "":
+#                 return link
+#     print 'error'
 
 def checkFirstLinkBottom(page):
-    links = re.findall("(?s)(?:<div class=\"mw-parser-output\")(?:.*?)|(<a href=\"(?P<url>/wiki/(?:.*?)?)\")|(?:<div(?! class=\"mw-parser-output\")(?:.*?)<\/div>|<div(?! class=\"mw-parser-output\")(?:.*?)<\/div>)|(?:<table class=\"infobox_v2\">(?:.*?)<\/table>)", page)
+    page = regex.sub("(?s).*<div class=\"mw-parser-output\">", "", page)
+    links = regex.findall("(?s)(?:<div(?:(?:(?:.+?(?=<div|<\/div))(?:(?!<div)|(?R))*+)*?)<\/div>)|(?:<a href=\"(?P<url>\/wiki\/(?:.*?)?)\")", page)
+    # print links
     for link in links:
         if not ':' in link:
             if link != "":
-                print link
-    print 'error'
+                return link
+
 
 def checkFirstLinkTop(page):
     links = re.findall("(?s)<div class=\"mw-parser-output\">(?:.*?)|(?:<a href=\"(?P<url>/wiki/(?:.*?)?)\")(?:.*?)<\/div>", page)
+
     for link in links:
         if not ':' in link:
             if link != "":
@@ -26,7 +44,9 @@ def checkFirstLinkTop(page):
     print 'error'
 
 if __name__ == "__main__":
-    url = "https://fr.wikipedia.org/wiki/Connaissances"
+    numberOfThreads = int(sys.argv[1])
+    verrou = threading.RLock()
+    url = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
     while (1):
         page = getPage(url)
         pageName = getPageName(page)
